db3cdd575375936d47a7edc02d89637355b5c163
jonathancawood <68835478+jonathancawood@users.noreply.github.com>
2021-05-24 12:35:10 +1000

    making sure its up to date

--- Stage_2.java
+++ Stage_2.java
@@ -6,17 +6,14 @@ public class Stage_2 {
     private static final String ADDRESS = "127.0.0.1";
     private static final int PORT = 50000;
     private static final String REDY = "REDY\n";
-    private static final String HELO = "HELO\n";
-    private static final String OK = "OK\n";
+    private static final String HELO = "HELO";
+    private static final String OK = "OK";
     private static final String NONE = "NONE\n";
-    private static final String AUTH = "AUTH jono/n";
+    private static final String AUTH = "AUTH jono";
     private static final String ERROR = "ERROR Something has gone wrong!!!!";
     private static final String QUIT = "QUIT\n";
-    //private static final String DSSYSTEM_FILE_ADDRESs = "ds-system.xml";
     private static final String WHITE_SPACE = " ";
     private static final String GETS_ALL = "GETS All\n";
-    //private static final String GETS_CAPABLE = "GETS Capable";
-
     private static final String DESIRED_SERVER_STATE = "idle";
 
     public static void sendMSG(String msg, DataOutputStream out) {
@@ -56,56 +53,9 @@ public class Stage_2 {
 		}
     }
 
-
-    //private static List<Server> parseDSSystemXML(String fileAddress){
-        //List<Server> dsServerList = new ArrayList<>(); 
-       	// for (int i = 0; i < numServer; i++) {
-		// 	rcvd = readMSG(din);
-		// 	String[] stringList = parsing(rcvd);
-		// 	serverList[i] = new Server(stringList[0], stringList[4]);
-		// }
-    
-        //create a Server class
-        //do the parsing here
-        //create job class, complete it with the reqired attributes like getter and setter functions
-        //create Server class, complete it with the required attributes like getter and setter functions 
-        //optional >> implement comparable for server class or job class
-        //easily sort those classes based on any attributes you want based collections java class
-        //return dsServerList;
-    //}   //not used
-
-    // private static List<String> parseJOBNMessage(String serverReply){
-    //     List<String> info = new ArrayList<>();
-    //     String[] splitInfo = serverReply.split(WHITE_SPACE);
-    //     info.add(splitInfo[2]);
-    //     info.add(splitInfo[4]);
-    //     info.add(splitInfo[5]);
-    //     info.add(splitInfo[6]);
-    //     return info;
-    // }   //not used (implment to convert string to job class, maybe store in a list)
-
     private static String createSCHDString(String jobId, int serverId, String serverType){
         return "SCHD" +WHITE_SPACE + jobId + WHITE_SPACE + serverType + WHITE_SPACE + serverId;
     }
-
-    // private static String[] hangleGetsCapable(int coreCount, int memory, int disk, DataInputStream din, DataOutputStream dout){
-    //     String GetsCapable = GETS_CAPABLE +WHITE_SPACE+ coreCount +WHITE_SPACE+ memory +WHITE_SPACE+ disk;
-    //     dout.write(GetsCapable.getBytes());
-    //     String reply = din.readLine();
-    //     String[] datareply =reply.split(WHITE_SPACE);
-    //     int lines = Integer.parseInt(datareply[1]);
-    //     int length = Integer.parseInt(datareply[2]);
-    //     //for (){                                                                                      //needs work
-    //         //read message from server                                                               //needs work
-    //         //parse and add it to the temporay servers                                               //needs work 
-    //     //}                                                                                            //needs work
-    //     //look for the best capcable server within temporry servers.                                 //needs work
-    //     String sId, stype;
-    //     String[] ans = new String[2];
-    //     ans[0]= sId;
-    //     ans[1]= stype;
-    //     return ans;
-    // }   // not used (using gets all)
 
     public static Server DescendingFirstFit(String[] job, Server[] serverlist){
         //the idea is to reverse the array and start with the biggest corecount first, first fit on steroids.
