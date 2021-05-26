1103e105c579de5bc3259534570ab203560b4dfe
jonathancawood <68835478+jonathancawood@users.noreply.github.com>
2021-05-26 12:19:56 +1000

    finished commiting

--- Job.java
+++ Job.java
@@ -1,68 +1,69 @@
+// This class is not used in the Stage_2 implementation but was created, in future implementation this Job class will be useful.
+
 public class Job {
-    private int submitTime;
-    private int jobID;
-    private int estRuntime;
-    private int core;
-    private int memory;
-    private int disk;
+    private int submitTime;     // the job submit time
+    private int jobID;          // the jobID
+    private int estRuntime;     // the estimated run time of the job
+    private int core;           // the core count of the job 
+    private int memory;         // the memory required
+    private int disk;           // the disk space
 
     public Job(int _submitTime, int _jobID, int _estRuntime, int _core, int _memory, int _disk){
-        //setters methods
-        setJOBsubmitTime(_submitTime);
-        setJOBjobID(_jobID);
-        setJOBestRuntime(_estRuntime);
-        setJOBcore(_core);
-        setJOBmemory(_memory);
-        setJOBdisk(_disk);
+        setJOBsubmitTime(_submitTime);         // call the setter method for submit time
+        setJOBjobID(_jobID);                   // call the setter method for job id 
+        setJOBestRuntime(_estRuntime);         // call the setter method for for estimated run time
+        setJOBcore(_core);                     // call the setter method for core
+        setJOBmemory(_memory);                 // call the setter method for memory
+        setJOBdisk(_disk);                     // call the setter method for disk 
     }
 
-    public void setJOBsubmitTime(int _submitTime){
+    public void setJOBsubmitTime(int _submitTime){    // setter method for job submit time 
         this.submitTime = _submitTime;
     }
 
-    public void setJOBjobID(int _jobID){
+    public void setJOBjobID(int _jobID){    // setter method for jobID
         this.jobID = _jobID;
     }
 
-    public void setJOBestRuntime(int _estRuntime){
+    public void setJOBestRuntime(int _estRuntime){    // setter method for job estimated run time 
         this.estRuntime = _estRuntime;
     }
 
-    public void setJOBcore(String _core){
+    public void setJOBcore(String _core){    // setter method for job cores 
         this.core = _core;
     }
 
-    public void setJOBmemory(int _memory){
+    public void setJOBmemory(int _memory){    // setter method for job memory
         this.memory = _memory;
     }
     
-    public void setJOBdisk(int _disk){
+    public void setJOBdisk(int _disk){    // setter method for job disk
         this.disk = _disk;
     }
 
 
 
-    public int getJOBsubmitTime(){
+    public int getJOBsubmitTime(){          // getter method for job submit time
         return this.submitTime;
     }
 
-    public int getJOBjobID(){
+    public int getJOBjobID(){          // getter method for job jobID
         return this.jobID;
     }
 
-    public int getJOBestRuntime(){
+    public int getJOBestRuntime(){          // getter method for job run time
         return this.estRuntime;
     }
 
-    public int getJOBcore(){
+    public int getJOBcore(){          // getter method for job core
         return this.core;
     }
 
-    public int getJOBmemory(){
+    public int getJOBmemory(){          // getter method for job memory
         return this.memory;
     }
 
-    public int getJOBdisk(){
+    public int getJOBdisk(){          // getter method for job disk
         return this.disk;
     }
 
